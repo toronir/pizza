ae4f4cfb55d03a664846da02933fd13779ed6262
E.Kilj <kiljanska.e@op.pl>
2023-07-09 18:13:32 +0200

    add_tools_end_env_variables

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 
-import "./index.css";
-import App from "./App";
-import store from "./store/store";
-import { Provider } from "react-redux";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import './index.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './store/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
